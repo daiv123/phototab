2f4ed1f4e748b0462e5ef994a9e639e620cc208a
Davin Clark <davinic2@illinois.edu>
2022-02-26 21:25:09 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,142 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, listAll, getDownloadURL} from "firebase/storage";
+import refreshSVG from './refresh.svg';
+import hamsterWheelSVG from './hamsterWheel.svg';
+import mailOpen from './mailOpen.svg';
+import mailClosed from './mailClosed.svg';
+
+const backgroundColors = [
+  "bg-rose-200",
+  "bg-purple-200",
+  "bg-fuchsia-200",
+  "bg-blue-200",
+  "bg-green-200",
+  "bg-yellow-200",
+  "bg-red-200",
+  "bg-orange-200",
+  "bg-teal-200",
+  "bg-pink-200"
+];
+
+function ImageDisplay(props) {
+  const [loading, setLoading] = useState(true);
+  const imageLoaded = () => {
+    setLoading(false);
+  };
+  return (
+    <div className='bg-transparent flex h-full w-full absolute items-center justify-center'>
+      <div className={loading ? "block" : "hidden"}>
+        <img className="w-full h-full animate-spin " src={hamsterWheelSVG} alt="hamster wheel" />
+      </div>
+      <div className={(loading ? "hidden" : "block") + " h-2/3 w-2/3 flex items-center justify-center"}>
+        <img className="w-auto h-auto rounded-lg object-contain" 
+          src={props.image} alt='hamster'
+          onLoad={imageLoaded}
+          />
+        </div>
+      </div>
+  )
+}
+
+
+
+function PopUpMessage() {
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => {
+    setShow(!show);
+  };
+  // icon that shows text box when clicked
+  return (
+    <div className="fixed bottom-0 left-0 m-4">
+      <div className={show ? "block" : "hidden"}>
+        <div className="bg-white rounded-lg shadow-lg p-4">
+          <div className="text-left max-w-screen-md ">
+            <h1 className="text-3xl font-bold mb-2">Happy Valentines Day Chloe!</h1>
+            <p className="text-xl mb-1">
+              I hope you like my hamster tab extension! It will show you a new hamster 
+              every time you open a new tab, and I can add any photo that you send me :)
+              Thank you for being super awesome and the best girlfriend ever! I love you!
+            </p>
+            <p className="text-xl">
+              Love,<br/>Davin &#10084;
+            </p>
+          </div>
+        </div>
+      </div>
+      <img className="w-10 h-10 cursor-pointer " src={show ? mailOpen : mailClosed} alt="refresh" onClick={handleClick} />
+    </div>
+  )
+}
+
 
 function App() {
+
+  const [image, setImage] = useState(null);
+  const [bgColor, setBgColor] = useState(null);
+
+
+  async function updateLocalStorage() {
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: "AIzaSyAPDfesqEkKNYsZyAiiTPC0P9ZDETDyNl4",
+      authDomain: "phototab-1644615816381.firebaseapp.com",
+      projectId: "phototab-1644615816381",
+      storageBucket: "phototab-1644615816381.appspot.com",
+      messagingSenderId: "185221892148",
+      appId: "1:185221892148:web:152bb2b7aeace78e396261",
+      measurementId: "G-9NX2VF1VY6"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const storage = getStorage(app);
+    const hamstersRef = ref(storage, 'hamsters');
+    const res = await listAll(hamstersRef);
+    const hamsters = res.items;
+    let urlList = [];
+    let promises = [];
+    hamsters.forEach(hamster => {
+      console.log(hamster)
+      promises.push(getDownloadURL(hamster)
+        .then((res) => {
+          urlList.push(res);
+        }).catch((error) => {
+          console.log(error);
+        }));
+    });
+    await Promise.all(promises);
+    console.log(urlList);
+    window.localStorage.setItem("hamster_image_urls", JSON.stringify(urlList));
+  }
+  function reloadImages() {
+    updateLocalStorage();
+    const hamsters = JSON.parse(window.localStorage.getItem("hamster_image_urls"));
+    console.log(hamsters.length);
+    setImage(hamsters[Math.floor(Math.random() * hamsters.length)]);
+    setBgColor(backgroundColors[Math.floor(Math.random() * backgroundColors.length)]);
+  }
+  useEffect(() => {
+    if (window.localStorage.getItem("hamster_image_urls") === null) {
+      updateLocalStorage();
+    }
+    const hamsters = JSON.parse(window.localStorage.getItem("hamster_image_urls"));
+    setImage(hamsters[Math.floor(Math.random() * hamsters.length)]);
+    setBgColor(backgroundColors[Math.floor(Math.random() * backgroundColors.length)]);
+  }, [])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"w-full h-full absolute " + bgColor} >
+      <ImageDisplay image={image}/>
+      <div className="absolute bottom-0 right-0 m-4">
+        <img className="w-10 h-10 cursor-pointer " src={refreshSVG} alt="refresh" onClick={reloadImages} />
+      </div>
+      <PopUpMessage/>
     </div>
   );
 }
